1e78525d8e21fd213753e8371a43fc86089ae09d
FEWoobIn <hwb0218@gmail.com>
2024-04-22 20:11:54 +0900

    feat: 회원가입 스키마 조건을 추가합니다.

--- src/schema/signup.ts
+++ src/schema/signup.ts
@@ -1,4 +1,4 @@
-import * as z from "zod";
+import { z } from "zod";
 
 export const schema = z
   .object({
@@ -7,20 +7,21 @@ export const schema = z
       .min(1, { message: "이름은 2글자 이상이어야 합니다." }),
     email: z
       .string({ required_error: "올바른 이메일을 입력해주세요." })
-      .min(1, { message: "이메일을 입력하세요" })
       .email({ message: "올바른 이메일을 입력해주세요." }),
     phoneNumber: z
       .string({ required_error: "연락처는 11자리여야 합니다." })
       .length(11, { message: "연락처는 11자리여야 합니다." })
-      .regex(/^010([0-9]{3,4})([0-9]{4})$/, { message: "" }),
+      .regex(/^010([0-9]{ 4})([0-9]{4})$/, { message: "010으로 시작하는 11자리 숫자를 입력해주세요" }),
     role: z.string({ required_error: "카테고리를 선택하세요" }).min(1),
     password: z
-      .string()
-      .min(1, { message: "비밀번호를 입력하세요" })
+      .string({ required_error: "비밀번호는 최소 6자리 이상이어야 합니다." })
+      .min(6, { message: "비밀번호는 최소 6자리 이상이어야 합니다." })
       .regex(/^(?=.*[a-zA-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}$/, {
         message: "8자 이상의 영어 대문자 또는 소문자, 숫자, 특수문자가 필요합니다"
       }),
-    checkPassword: z.string().optional()
+    checkPassword: z
+      .string({ required_error: "비밀번호는 최소 6자리 이상이어야 합니다." })
+      .min(6, { message: "비밀번호는 최소 6자리 이상이어야 합니다." })
   })
   .superRefine(({ password, checkPassword }, ctx) => {
     if (password !== checkPassword) {
