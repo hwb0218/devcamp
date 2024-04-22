b2435553e5d6096dcdfe6a3e73c046feee141372
FEWoobIn <hwb0218@gmail.com>
2024-04-22 21:24:38 +0900

    feat: Step2 입력 필드 작업

--- src/app/_component/signup-form/Step2.tsx
+++ src/app/_component/signup-form/Step2.tsx
@@ -1,12 +1,44 @@
-import React, { Dispatch, SetStateAction } from "react";
+import React from "react";
 import { UseFormReturn } from "react-hook-form";
 
-import { Schema } from "../../../schema/signup";
+import { FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+import { Schema } from "@/schema/signup";
 
 interface Props {
   form: UseFormReturn<Schema>;
 }
 
 export default function Step2({ form }: Props) {
-  return <div>Step2</div>;
+  return (
+    <>
+      <FormField
+        control={form.control}
+        name="password"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>비밀번호</FormLabel>
+            <FormControl>
+              <Input type="password" {...field} className="w-full" />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name="checkPassword"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>비밀번호 확인</FormLabel>
+            <FormControl>
+              <Input type="password" className="w-full" {...field} />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+    </>
+  );
 }
