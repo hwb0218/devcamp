96e0e75ef47304691323f049cc7c1fc5115a7742
FEWoobIn <hwb0218@gmail.com>
2024-04-24 19:07:58 +0900

    fix: Extra attributes from the server: class, style 에러

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,9 +1,19 @@
 "use client";
 
-import React, { PropsWithChildren } from "react";
+import React, { useState, useEffect, PropsWithChildren } from "react";
 import { ThemeProvider } from "next-themes";
 
 export default function Providers({ children }: PropsWithChildren) {
+  const [isMount, setMount] = useState(false);
+
+  useEffect(() => {
+    setMount(true);
+  }, []);
+
+  if (!isMount) {
+    return null;
+  }
+
   return (
     <ThemeProvider attribute="class" defaultTheme="light">
       {children}
