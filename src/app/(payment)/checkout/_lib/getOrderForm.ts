1c1504f0a229aadb500e059671a5e27e4f8909e2
FEWoobIn <hwb0218@gmail.com>
2024-05-01 05:24:27 +0900

    feat: getOrderForm API

--- src/app/(payment)/checkout/_lib/getOrderForm.ts
+++ src/app/(payment)/checkout/_lib/getOrderForm.ts
@@ -1,18 +1,36 @@
 export interface OrderForm {
   shippingAddress: ShippingAddress;
+  coupon: Coupon[];
+  itemList: ItemList[];
 }
 
 export interface ShippingAddress {
   name: string;
   receiver: string;
-  zipCode: string;
-  mainAddress: string;
-  subAddress: string;
+  address: string;
   mainPhoneNumber: string;
 }
 
+export interface Coupon {
+  label: string;
+  value: number;
+  type: string;
+}
+
+export interface ItemList {
+  brandName: string;
+  itemId: number;
+  itemName: string;
+  itemImage: string;
+  itemPrice: number;
+  orderCount: number;
+  option: string;
+}
+
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+
 export const getOrderForm = async (): Promise<OrderForm> => {
-  const res = await fetch("http://localhost:3000/data/order.json", { method: "GET", cache: "no-store" });
+  const res = await fetch(`${BASE_URL}/data/order.json`, { method: "GET", cache: "no-store" });
 
   if (!res.ok) {
     throw new Error("Failed to fetch data");
