0f09e9a0234966f7aef1556c5c325fe044fd97fe
FEWoobIn <hwb0218@gmail.com>
2024-05-01 23:18:27 +0900

    feat: 총 할인금액 selector

--- src/recoil/orderFormAtom.ts
+++ src/recoil/orderFormAtom.ts
@@ -1,7 +1,17 @@
-import { OrderForm } from "@/app/(payment)/checkout/_lib/getOrderForm";
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 
-export const orderFormAtom = atom<OrderForm>({
+import type { Coupon, ItemList, OrderForm } from "@/app/(payment)/checkout/_lib/getOrderForm";
+
+export interface NewItemList extends ItemList {
+  discountPrice: number;
+}
+
+export interface OrderFormAtom extends OrderForm {
+  selectedCoupon?: Coupon;
+  itemList: NewItemList[];
+}
+
+export const orderFormAtom = atom<OrderFormAtom>({
   key: "orderFormAtom",
   default: {
     shippingAddress: {
@@ -11,6 +21,19 @@ export const orderFormAtom = atom<OrderForm>({
       mainPhoneNumber: ""
     },
     coupon: [],
-    itemList: []
+    itemList: [],
+    mileage: 0,
+    selectedCoupon: { label: "", value: 0, type: "" }
+  }
+});
+
+export const totalDiscountPriceSelector = selector({
+  key: "totalDiscountPriceSelector",
+  get: ({ get }) => {
+    const { itemList } = get(orderFormAtom);
+
+    return itemList.reduce((acc, cur) => {
+      return acc + cur.itemPrice * cur.orderCount - cur.discountPrice;
+    }, 0);
   }
 });
